4435cdee0a5b000fe6c0da5c2b716ec311c0e08d
nitsvutt <nitsvutt@gmail.com>
2023-07-25 09:59:48 +0700

    consecutive_days example

--- sql/self-join/consecutive_days/consecutive_days.sql
+++ sql/self-join/consecutive_days/consecutive_days.sql
@@ -2,13 +2,13 @@ SELECT ROUND(
     (
         (
             SELECT COUNT(DISTINCT a1.player_id)
-            FROM Activity a1
-            INNER JOIN Activity a2
+            FROM activity a1
+            INNER JOIN activity a2
             ON a1.player_id = a2.player_id
             AND a1.event_date = DATE_ADD(a2.event_date, INTERVAL 1 DAY)
         )
         /
         (
-            SELECT COUNT(DISTINCT player_id) FROM Activity
+            SELECT COUNT(DISTINCT player_id) FROM activity
         )
     ), 2) AS fraction;
