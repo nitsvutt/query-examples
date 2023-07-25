deeb9707b2fa2a83d45557bfa484021d7f2d29be
nitsvutt <nitsvutt@gmail.com>
2023-07-25 09:35:59 +0700

    relevant_users example

--- spark/left-semi-join/relevant_users/relevant_users.py
+++ spark/left-semi-join/relevant_users/relevant_users.py
@@ -1,5 +1,5 @@
-pupil.join(innovation_craft.where(col("type") == 1),
-           pupil["id"] == innovation_craft["owner_id"],
+pupil.join(innovationCraft.where(col("type") == 1),
+           pupil["id"] == innovationCraft["owner_id"],
            "left_semi") \
      .select("id", "first_name", "last_name", "grade") \
      .orderBy("grade", "id") \
--- spark/left-semi-join/relevant_users/relevant_users.scala
+++ spark/left-semi-join/relevant_users/relevant_users.scala
@@ -1,5 +1,5 @@
-pupil.join(innovation_craft.where(col("type") === 1),
-           pupil.col("id") === innovation_craft.col("owner_id"),
+pupil.join(innovationCraft.where(col("type") === 1),
+           pupil.col("id") === innovationCraft.col("owner_id"),
            "left_semi")
      .select("id", "first_name", "last_name", "grade")
      .orderBy("grade", "id")
