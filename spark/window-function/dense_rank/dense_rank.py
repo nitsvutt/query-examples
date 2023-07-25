fbf109d11e2a05a1d22dbb9099ddd671250cb017
nitsvutt <nitsvutt@gmail.com>
2023-07-25 10:01:15 +0700

    dense_ranke example

--- spark/window-function/dense_rank/dense_rank.py
+++ spark/window-function/dense_rank/dense_rank.py
@@ -1,11 +1,11 @@
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 
-windowSpec = Window.partitionBy("grade") \
+window_spec = Window.partitionBy("grade") \
                    .orderBy("gpa") \
                    .rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
-ranked_pupil = pupil.withColumn("rank", dense_rank().over(windowSpec))
+ranked_pupil = pupil.withColumn("rank", dense_rank().over(window_spec))
 
 ranked_pupil.select("id", "first_name", "last_name", "grade", "gpa") \
             .where(col("rank") == 1) \
--- spark/window-function/dense_rank/dense_rank.scala
+++ spark/window-function/dense_rank/dense_rank.scala
@@ -1,11 +1,11 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 
-val windowSpec = Window.partitionBy("grade")
+val window_spec = Window.partitionBy("grade")
                    .orderBy("gpa")
                    .rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
-val ranked_pupil = pupil.withColumn("rank", dense_rank().over(windowSpec))
+val ranked_pupil = pupil.withColumn("rank", dense_rank().over(window_spec))
 
 ranked_pupil.select("id", "first_name", "last_name", "grade", "gpa")
             .where(col("rank") === 1)
